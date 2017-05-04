2624b810fe348e9ef8a7a69af7cb0f65236bef68
AleksandrOrlov <orlovayu92@gmail.com>
2017-05-04 15:16:19 +0700

    добавил NLog + config

--- UtilsAgent.Test/MainTest.cs
+++ UtilsAgent.Test/MainTest.cs
@@ -1,6 +1,13 @@
-﻿using Akka.Configuration;
+﻿using System;
+using Akka.Configuration;
+using Akka.Dispatch.SysMsg;
+using Akka.Event;
 using Akka.TestKit.NUnit3;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NLog;
+using NLog.Targets;
+using NUnit.Framework;
+using UtilsAgent.CompanyCloning;
 
 namespace UtilsAgent.Test
 {
@@ -18,14 +25,23 @@ namespace UtilsAgent.Test
         {
             //var container = new UnityContainer();
             //container.RegisterType<ISourceData, SourceData>();
-
+            
             //Sys.AddDependencyResolver(new UnityDependencyResolver(container, Sys));
 
             //var a = ActorOf(Sys.DI().Props<CloneFullCompany>(), "Worker1");
-
+            
             //a.Tell(new StartCloningCompany("asf"), null);
+            
 
-            ExpectNoMsg();
+            var message = "fsa";
+
+            var testActor = ActorOf<TestMsgActor>("Worker1");
+
+            Within(TimeSpan.FromSeconds(1), () =>
+            {
+                testActor.Tell(message, TestActor);
+                ExpectMsg<string>((msg, sender) => msg == message + " OK");
+            });
         }
     }
 }
