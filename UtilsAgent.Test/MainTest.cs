973a486b8c36353924e883b1e0e58a5c235d2ffd
AleksandrOrlov <orlovayu92@gmail.com>
2017-05-04 12:19:52 +0700

    init proj

---
+++ UtilsAgent.Test/MainTest.cs
@@ -0,0 +1,31 @@
+﻿using Akka.Configuration;
+using Akka.TestKit.NUnit3;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UtilsAgent.Test
+{
+    [TestClass]
+    public class MainTest : TestKit
+    {
+        public MainTest()
+            : base(ConfigurationFactory.Load().WithFallback(DefaultConfig))
+        {
+
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //var container = new UnityContainer();
+            //container.RegisterType<ISourceData, SourceData>();
+
+            //Sys.AddDependencyResolver(new UnityDependencyResolver(container, Sys));
+
+            //var a = ActorOf(Sys.DI().Props<CloneFullCompany>(), "Worker1");
+
+            //a.Tell(new StartCloningCompany("asf"), null);
+
+            ExpectNoMsg();
+        }
+    }
+}
